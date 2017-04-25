ec7152a63ad49d907fd677b288024c81633d6340
gkandemi <gokhan@gkandemir.com>
2017-04-25 15:46:19 +0300

    mesaj gondermek için front-end form eklendi

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,19 @@ socket.on("message", function(message){
     console.log("New Message");
     console.log(message.text);
 })
+
+var $form = jQuery("form#message_form");
+
+$form.on("submit", function(event){
+
+    event.preventDefault();
+
+    var $message = $form.find("input[name='message']");
+    socket.emit('message', {
+        text: $message.val()
+    })
+
+    $message.val("");
+    $message.focus();
+
+})
